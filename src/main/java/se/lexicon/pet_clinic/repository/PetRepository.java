19369bf6d63fef9e8fe8c1c563a55ba98e72b1a7
Sebastian Bocaciu <75611075+TheAppleJuice@users.noreply.github.com>
2021-04-28 10:31:54 +0200

    Added PetRepository CRUD + todo

--- src/main/java/se/lexicon/pet_clinic/repository/PetRepository.java
+++ src/main/java/se/lexicon/pet_clinic/repository/PetRepository.java
@@ -1,12 +1,23 @@
 package se.lexicon.pet_clinic.repository;
 
 
-public interface PetRepository {
+import org.springframework.data.repository.CrudRepository;
+import se.lexicon.pet_clinic.entity.Owner;
+import se.lexicon.pet_clinic.entity.Pet;
+import se.lexicon.pet_clinic.entity.PetType;
 
-    // todo: implement basic CRUD
+import java.util.List;
+
+public interface PetRepository extends CrudRepository<Pet, String> {
+
+    // todo: implement basic CRUD. Done
 
     //todo: select pet by name
+    List<Pet> findPetByNameIgnoreCase (String name);
     //todo: select pet by pet type name
+    List<Pet> findPetByPetTypeNameIgnoreCase (PetType name);
     //todo: select pet by owner first name and last name
+    List<Pet> findPetByOwner_FirstNameIgnoreCaseAndOwner_LastNameIgnoreCase (Owner firstName, Owner lastName);
     //todo: select pet by owner telephone
+    List<Pet> findPetByOwner_Telephone (Owner telephone);
 }
